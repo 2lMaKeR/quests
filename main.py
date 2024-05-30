413821552a386d3db4f7cfa17ac563fc99d2f6b1
Felipe Tomé or 2lMaKeR <120343427+2lMaKeR@users.noreply.github.com>
2024-05-30 01:21:00 -0300

    adicionando métodos de visualização e seleção das variáveis

--- main.py
+++ main.py
@@ -13,13 +13,13 @@ import random
 item=['flor','cordão','balde']
 contratante=['Moça','Padre','Construtor']
 local=['Capital','Campos Verdejantes','Gabara']
-localE=['residência Mill','Igreja','ponte Strefield']
+local_esp=['residência Mill','Igreja','ponte Strefield']
 
 #randomizer
 item_random=random.choice(item)
 contratante_random=random.choice(contratante)
 local_random=random.choice(local)
-localE_random=random.choice(localE)
+local_esp_random=random.choice(local_esp)
 
 #lock
 its=0
@@ -32,7 +32,7 @@ lcs=0
 local_selec=[lcs]
 
 lces=0
-localE_selec=[lces]
+local_esp_selec=[lces]
 
 # -- -- --
 #concluido
@@ -41,7 +41,7 @@ localE_selec=[lces]
 def mostrar_random():
     print('\n########################\n')
     print('Sua quest será...\n')
-    print('Entregue {} para {} na {} na {}.'.format(item_random,contratante_random,localE_random,local_random))
+    print('Entregue {} para {} na {} na {}.'.format(item_random,contratante_random,local_esp_random,local_random))
     print('\n########################')
 
 
@@ -82,6 +82,122 @@ def lock_itens():
     mostrar_item()
     escolher_item()
 
+# -- -- --
+#contratante
+
+#listar itens cadastrados
+def mostrar_contratante():
+    cont_it=1
+    print('Selecione 0 para retornar as opções.\n')
+    for contratantes in contratante:
+        nome_contratante=contratantes
+        print(f'{cont_it}. {nome_contratante}')
+        cont_it+=1
+  
+
+#escolha de itens
+def escolher_contratante():
+    verd=len(contratante)
+    its=int(input('Escolha o contratante que irá fazer a quest: '))
+    if cts<=verd and cts>0:
+        contratante_selec[0]=(cts-1)
+        pr=(contratante[contratante_selec[0]]).upper()
+        print('Contratante {} selecionado!!'.format(pr))
+        voltando_ao_lock()
+    elif its==0:
+        voltando_ao_lock()
+    else:
+        print('\nOpção inválida!\n')
+        escolher_contratante()
+
+#cadastrar novo item
+def cadastrar_contratante():
+    novo_contratante=str(input('Qual novo contratante gostaria de cadastrar: '))
+    contratante.append(novo_contratante)
+
+#mostrando opcoes de lock de itens
+def lock_contratantes():
+    mostrar_contratante()
+    escolher_contratante()
+
+
+# -- -- --
+#local
+
+#listar itens cadastrados
+def mostrar_local():
+    cont_it=1
+    print('Selecione 0 para retornar as opções.\n')
+    for locais in local:
+        nome_local=locais
+        print(f'{cont_it}. {nome_local}')
+        cont_it+=1
+  
+
+#escolha de itens
+def escolher_local():
+    verd=len(local)
+    lcs=int(input('Escolha o local que irá fazer a quest: '))
+    if lcs<=verd and lcs>0:
+        local_selec[0]=(lcs-1)
+        pr=(local[local_selec[0]]).upper()
+        print('Local {} selecionado!!'.format(pr))
+        voltando_ao_lock()
+    elif its==0:
+        voltando_ao_lock()
+    else:
+        print('\nOpção inválida!\n')
+        escolher_local()
+
+#cadastrar novo item
+def cadastrar_local():
+    novo_local=str(input('Qual novo local gostaria de cadastrar: '))
+    local.append(novo_local)
+
+#mostrando opcoes de lock de itens
+def lock_local():
+    mostrar_local()
+    escolher_local()
+
+
+# -- -- --
+#local E
+
+#listar itens cadastrados
+def mostrar_local_esp():
+    cont_it=1
+    print('Selecione 0 para retornar as opções.\n')
+    for locais_esp in local_esp:
+        nome_local_esp=locais_esp
+        print(f'{cont_it}. {nome_local_esp}')
+        cont_it+=1
+  
+
+#escolha de itens
+def escolher_local_esp():
+    verd=len(local_esp)
+    lces=int(input('Escolha o local específico que irá fazer a quest: '))
+    if lces<=verd and lces>0:
+        local_esp_selec[0]=(lces-1)
+        pr=(local_esp[local_esp_selec[0]]).upper()
+        print('Local específico {} selecionado!!'.format(pr))
+        voltando_ao_lock()
+    elif its==0:
+        voltando_ao_lock()
+    else:
+        print('\nOpção inválida!\n')
+        escolher_local_esp()
+
+#cadastrar novo item
+def cadastrar_local_esp():
+    novo_local_esp=str(input('Qual novo local específico gostaria de cadastrar: '))
+    local_esp.append(novo_local_esp)
+
+#mostrando opcoes de lock de itens
+def lock_local_esp():
+    mostrar_local_esp()
+    escolher_local_esp()
+
 
 # -- -- --
 #locks
@@ -102,11 +218,11 @@ def selecionar_locks():
         elif opcao_escolhida==2:
             lock_itens()
         elif opcao_escolhida==3:
-            print('mostrar opcoes de lock')
+            lock_contratantes()
         elif opcao_escolhida==4:
-            print('mostrar opcoes de lock')
+            lock_local()
         elif opcao_escolhida==5:
-            print('mostrar opcoes de lock')
+            lock_local_esp()
         elif opcao_escolhida==6:
             voltando_ao_menu()
         else:
