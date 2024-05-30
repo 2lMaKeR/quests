9770791e442b609860a8c1271556fec50f19b8e0
Felipe Tomé or 2lMaKeR <120343427+2lMaKeR@users.noreply.github.com>
2024-05-30 02:11:36 -0300

    refatorando sistema de escolhas e exibicao

--- main.py
+++ main.py
@@ -48,30 +48,6 @@ def mostrar_random():
 # -- -- --
 #itens
 
-#listar itens cadastrados
-def mostrar_item():
-    cont_it=1
-    print('Selecione 0 para retornar as opções.\n')
-    for itens in item:
-        nome_item=itens
-        print(f'{cont_it}. {nome_item}')
-        cont_it+=1
-  
-
-#escolha de itens
-def escolher_item():
-    verd=len(item)
-    its=int(input('Escolha o item que irá fazer a quest: '))
-    if its<=verd and its>0:
-        item_selec[0]=(its-1)
-        pr=(item[item_selec[0]]).upper()
-        print('Item {} selecionado!!'.format(pr))
-    elif its==0:
-        voltando_ao_lock()
-    else:
-        print('\nOpção inválida!\n')
-        escolher_item()
-
 #cadastrar novo item
 def cadastrar_item():
     novo_item=str(input('Qual novo item gostaria de cadastrar: '))
@@ -84,31 +60,6 @@ def lock_itens():
 
 # -- -- --
 #contratante
-
-#listar itens cadastrados
-def mostrar_contratante():
-    cont_it=1
-    print('Selecione 0 para retornar as opções.\n')
-    for contratantes in contratante:
-        nome_contratante=contratantes
-        print(f'{cont_it}. {nome_contratante}')
-        cont_it+=1
-  
-
-#escolha de itens
-def escolher_contratante():
-    verd=len(contratante)
-    its=int(input('Escolha o contratante que irá fazer a quest: '))
-    if cts<=verd and cts>0:
-        contratante_selec[0]=(cts-1)
-        pr=(contratante[contratante_selec[0]]).upper()
-        print('Contratante {} selecionado!!'.format(pr))
-        voltando_ao_lock()
-    elif its==0:
-        voltando_ao_lock()
-    else:
-        print('\nOpção inválida!\n')
-        escolher_contratante()
 
 #cadastrar novo item
 def cadastrar_contratante():
@@ -125,29 +76,6 @@ def lock_contratantes():
 #local
 
 #listar itens cadastrados
-def mostrar_local():
-    cont_it=1
-    print('Selecione 0 para retornar as opções.\n')
-    for locais in local:
-        nome_local=locais
-        print(f'{cont_it}. {nome_local}')
-        cont_it+=1
-  
-
-#escolha de itens
-def escolher_local():
-    verd=len(local)
-    lcs=int(input('Escolha o local que irá fazer a quest: '))
-    if lcs<=verd and lcs>0:
-        local_selec[0]=(lcs-1)
-        pr=(local[local_selec[0]]).upper()
-        print('Local {} selecionado!!'.format(pr))
-        voltando_ao_lock()
-    elif its==0:
-        voltando_ao_lock()
-    else:
-        print('\nOpção inválida!\n')
-        escolher_local()
 
 #cadastrar novo item
 def cadastrar_local():
@@ -164,29 +92,6 @@ def lock_local():
 #local E
 
 #listar itens cadastrados
-def mostrar_local_esp():
-    cont_it=1
-    print('Selecione 0 para retornar as opções.\n')
-    for locais_esp in local_esp:
-        nome_local_esp=locais_esp
-        print(f'{cont_it}. {nome_local_esp}')
-        cont_it+=1
-  
-
-#escolha de itens
-def escolher_local_esp():
-    verd=len(local_esp)
-    lces=int(input('Escolha o local específico que irá fazer a quest: '))
-    if lces<=verd and lces>0:
-        local_esp_selec[0]=(lces-1)
-        pr=(local_esp[local_esp_selec[0]]).upper()
-        print('Local específico {} selecionado!!'.format(pr))
-        voltando_ao_lock()
-    elif its==0:
-        voltando_ao_lock()
-    else:
-        print('\nOpção inválida!\n')
-        escolher_local_esp()
 
 #cadastrar novo item
 def cadastrar_local_esp():
@@ -201,13 +106,121 @@ def lock_local_esp():
 
 # -- -- --
 #locks
+def mostrar_locks(lock_cat):
+    if lock_cat=='item':
+        cont_it=1
+        print(retorno_ops)
+        for itens in item:
+            nome_item=itens
+            print(f'{cont_it}. {nome_item}')
+            cont_it+=1
+
+    elif lock_cat=='contratante':
+        cont_it=1
+        print(retorno_ops)
+        for contratantes in contratante:
+            nome_contratante=contratantes
+            print(f'{cont_it}. {nome_contratante}')
+            cont_it+=1
+
+    elif lock_cat=='local':
+        cont_it=1
+        print(retorno_ops)
+        for locais in local:
+            nome_local=locais
+            print(f'{cont_it}. {nome_local}')
+            cont_it+=1
+
+    elif lock_cat=='local especifico':
+        cont_it=1
+        print(retorno_ops)
+        for locais_esp in local_esp:
+            nome_local_esp=locais_esp
+            print(f'{cont_it}. {nome_local_esp}')
+            cont_it+=1
+
+    else:
+        print('#######################')
+        print('ERRO def mostrar_locks')
+        print('#######################')
+
+
+def escolher_locks(lock_cat):
+    if lock_cat=='item':
+        verd=len(item)
+        its=int(input('Escolha o item que irá fazer a quest: '))
+        if its<=verd and its>0:
+            item_selec[0]=(its-1)
+            pr=(item[item_selec[0]]).upper()
+            print('Item {} selecionado!!'.format(pr))
+            voltando('lockar opções')
+        elif its==0:
+            voltando('lockar opções')
+        else:
+            print(op_inval)
+            escolher_locks('item')
+
+    elif lock_cat=='contratante':
+        verd=len(contratante)
+        its=int(input('Escolha o contratante que irá fazer a quest: '))
+        if cts<=verd and cts>0:
+            contratante_selec[0]=(cts-1)
+            pr=(contratante[contratante_selec[0]]).upper()
+            print('Contratante {} selecionado!!'.format(pr))
+            voltando('lockar opções')
+        elif its==0:
+            voltando('lockar opções')
+        else:
+            print(op_inval)
+            escolher_locks('contratante')
+
+    elif lock_cat=='local':
+        verd=len(local)
+        lcs=int(input('Escolha o local que irá fazer a quest: '))
+        if lcs<=verd and lcs>0:
+            local_selec[0]=(lcs-1)
+            pr=(local[local_selec[0]]).upper()
+            print('Local {} selecionado!!'.format(pr))
+            voltando('lockar opções')
+        elif its==0:
+            voltando('lockar opções')
+        else:
+            print(op_inval)
+            escolher_locks('local')
+
+    elif lock_cat=='local especifico':
+        verd=len(local_esp)
+        lces=int(input('Escolha o local específico que irá fazer a quest: '))
+        if lces<=verd and lces>0:
+            local_esp_selec[0]=(lces-1)
+            pr=(local_esp[local_esp_selec[0]]).upper()
+            print('Local específico {} selecionado!!'.format(pr))
+            voltando('lockar opções')
+        elif its==0:
+            voltando('lockar opções')
+        else:
+            print(op_inval)
+            escolher_locks('local especifico')
+    else:
+        print('#######################')
+        print('ERRO def escolher_locks')
+        print('#######################')
+
+#mostrando opcoes de lock de itens
+def fixar_locks(look_cat):
+    mostrar_locks(look_cat)
+    escolher_locks(look_cat)
+
+# -- -- --
+#locks
 
 def exibir_locks():
-    print('1. Lockar tipo de quest')
-    print('2. Lockar itens')
-    print('3. Lockar contratante')
-    print('4. Lockar Local')
-    print('5. Lockar Local específico')
+    print('Selecione a variável de que desejar fixar:')
+    print('1. Tipo de quest')
+    print('2. Item')
+    print('3. Contratante')
+    print('4. Local')
+    print('5. Local específico')
     print('6. Voltar...')
 
 def selecionar_locks():
@@ -216,19 +229,19 @@ def selecionar_locks():
         if opcao_escolhida==1:
             print('mostrar opcoes de lock')
         elif opcao_escolhida==2:
-            lock_itens()
+            fixar_locks('item')
         elif opcao_escolhida==3:
-            lock_contratantes()
+            fixar_locks('contratante')
         elif opcao_escolhida==4:
-            lock_local()
+            fixar_locks('local')
         elif opcao_escolhida==5:
-            lock_local_esp()
-        elif opcao_escolhida==6:
-            voltando_ao_menu()
+            fixar_locks('local especifico')
+        elif opcao_escolhida==6 or opcao_escolhida==0:
+            voltando('menu principal')
         else:
-            opcao_invalida(lock)
+            opcao_invalida('lock')
     except:
-        opcao_invalida(lock)
+        opcao_invalida('lock')
 
 
 #conjunto locks
@@ -255,43 +268,51 @@ def selecionar_opcoes():
             locks()
         elif opcao_escolhida==3:
             print('mostrar opcoes de lock')
-        elif opcao_escolhida==4:
+        elif opcao_escolhida==4 or opcao_escolhida==0:
             finalizar_app()
         else:
-            opcao_invalida(menu)
+            opcao_invalida('menu')
     except:
-        opcao_invalida(menu)
+        opcao_invalida('menu')
     
 
 # -- -- --
-#voltar e finalizando
+#voltar e finalizando  
 
-#voltar ao menu
-def voltando_ao_menu():
-    input('\nDigite qualquer tecla para voltar ao menu principal...')
-    main()
+#voltando
+def voltando(volta):
+    if volta=='menu principal':
+        input('\nDigite qualquer tecla para voltar ao {}...'.format(volta))
+        main()
+    elif volta=='lockar opções':
+        input('\nDigite qualquer tecla para voltar ao {}...'.format(volta))
+        locks()
+    else:
+        print('#######################')
+        print('ERRO def voltando')
+        print('#######################')
 
-#voltar ao lockar opçoes
-def voltando_ao_lock():
-    input('\nDigite qualquer tecla para voltar ao lockar itens...')
-    locks()
 
 #reiniciando sessões por invalidez
 def opcao_invalida(cat):
     print('\nOpção inválida!\n')
-    input('reiciando opções...\n')
     #adicionar clear
     if cat=='menu':
-        main()
+        voltando('menu principal')
     elif cat=='lock':
-        locks()
+        voltando('lockar opções')
     else:
-        print('\nERRO {OPÇÃO INVÁLIDA}!!')
+        print('#######################')
+        print('ERRO def opcao_invalida')
+        print('#######################')
 
 #finalizar app
 def finalizar_app():
     print('\n########################\n')
     print('Encerrando o programa...\n')
+
+retorno_ops='Selecione 0 para retornar as opções.\n'
+op_inval='\nOpção inválida!\n'
 
 
 #-- -- --
